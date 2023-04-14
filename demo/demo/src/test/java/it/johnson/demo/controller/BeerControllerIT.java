22e3e20d9deb4082b28bde1300783e8c0ead1428
MukengeNJ <john.mukenge>
2023-04-14 17:51:06 +0200

    Testing for expected Exceptions

--- demo/demo/src/test/java/it/johnson/demo/controller/BeerControllerIT.java
+++ demo/demo/src/test/java/it/johnson/demo/controller/BeerControllerIT.java
@@ -1,5 +1,6 @@
 package it.johnson.demo.controller;
 
+import it.johnson.demo.entities.Beer;
 import it.johnson.demo.model.BeerDTO;
 import it.johnson.demo.repositories.BeerRepository;
 import jakarta.transaction.Transactional;
@@ -9,8 +10,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 
 import java.util.List;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class BeerControllerIT {
@@ -20,6 +23,21 @@ class BeerControllerIT {
 
     @Autowired
     BeerRepository beerRepository;
+
+    @Test
+    void testBeerIdNotFound(){
+        assertThrows(NotFoundException.class, () -> {
+           beerController.getBeerById(UUID.randomUUID());
+        });
+    }
+
+    @Test
+    void testGetById(){
+        Beer beer = beerRepository.findAll().get(0);
+
+        BeerDTO beerDTO = beerController.getBeerById(beer.getId());
+        assertThat(beerDTO).isNotNull();
+    }
 
     @Test
     void testListBeers(){
